379218f6434e3a7700086be5067bec87ede9f9e1
Tai Le <sirlevantai@gmail.com>
2015-10-16 00:35:41 +0700

    Remove import in header file

--- UICommon/UICommon.h
+++ UICommon/UICommon.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double UICommonVersionNumber;
 FOUNDATION_EXPORT const unsigned char UICommonVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <UICommon/PublicHeader.h>
-
-#import <Utils/Utils.h>
