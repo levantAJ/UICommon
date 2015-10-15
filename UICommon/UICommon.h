77e114b2b440bd30d3322ac7f55bc2751397dd9e
Tai Le <sirlevantai@gmail.com>
2015-10-15 23:19:16 +0700

    .gitignore is now working

---
+++ UICommon/UICommon.h
@@ -0,0 +1,19 @@
+//
+//  UICommon.h
+//  UICommon
+//
+//  Created by Le Tai on 10/15/15.
+//  Copyright © 2015 AJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UICommon.
+FOUNDATION_EXPORT double UICommonVersionNumber;
+
+//! Project version string for UICommon.
+FOUNDATION_EXPORT const unsigned char UICommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UICommon/PublicHeader.h>
+
+
